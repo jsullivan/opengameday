86aec77f478989d5250ba062af6e3f7576f9364c
Josh Sullivan <josh@dipperstove.com>
2012-07-12 09:33:39 -0700

    Initial commit
    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Opengameday::Application.config.secret_token = '2f635747010d9331b7efd3d41b42f85cad6d9b83fb48900354535a32397816c3a6ccbee151d35da4c944db777d1c281b6fa3ee7ac9d7f3cccac0fe999225d575'
